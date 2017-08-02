3e4ae7aeab1c7a341269c668c4c56a1ea29cc4b3
Ivan Driuk <veytarn@gmail.com>
2017-08-02 07:17:11 +0300

    add credentials input and changeset to user

--- lib/rep/accounts/accounts.ex
+++ lib/rep/accounts/accounts.ex
@@ -6,7 +6,7 @@ defmodule Rep.Accounts do
   import Ecto.Query, warn: false
   alias Rep.Repo
 
-  alias Rep.Accounts.User
+  alias Rep.Accounts.{User, Credential}
 
   @doc """
   Returns the list of users.
@@ -58,6 +58,7 @@ defmodule Rep.Accounts do
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.insert()
   end
 
@@ -76,6 +77,7 @@ defmodule Rep.Accounts do
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.update()
   end
 
