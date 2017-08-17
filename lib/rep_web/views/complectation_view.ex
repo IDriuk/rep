66816ef0a737adbdc37ef146d9f6dc7d150f147c
Ivan Driuk <veytarn@gmail.com>
2017-08-17 11:28:17 +0300

    show complectations address, create link to address from short address, change back to cancel

--- lib/rep_web/views/complectation_view.ex
+++ lib/rep_web/views/complectation_view.ex
@@ -1,3 +1,10 @@
 defmodule RepWeb.ComplectationView do
   use RepWeb, :view
+
+  alias Rep.Lifts.Address
+
+  def get_short_address address do
+    %Address{street: street, house: house, entrance: entrance} = address
+    street <> " " <> to_string(house) <> " " <> to_string(entrance)
+  end
 end
