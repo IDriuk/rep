fcb05c707a680ecd4d248ca5dc7f27481dc559c5
Ivan Driuk <veytarn@gmail.com>
2017-08-21 07:53:09 +0300

    update translate

--- lib/rep_web/controllers/session_controller.ex
+++ lib/rep_web/controllers/session_controller.ex
@@ -11,7 +11,7 @@ defmodule RepWeb.SessionController do
     case Accounts.authenticate_by_email_password(email, password) do
       {:ok, user} ->
         conn
-        |> put_flash(:info, "Welcome back!")
+        |> put_flash(:info, gettext("Welcome back!"))
         |> put_session(:user_id, user.id)
         |> configure_session(renew: true)
         |> redirect(to: "/")
